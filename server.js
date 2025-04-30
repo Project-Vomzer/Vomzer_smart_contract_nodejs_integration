f0b6a61b1a01a3664b1e30f97251d4b240740870
Adetayo <twinebravo@gmail.com>
2025-04-30 21:24:06 +0100

    Updated server.js to use POrT in .env file

--- server.js
+++ server.js
@@ -307,6 +307,10 @@ app.post('/api/expend-reward', async (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// const PORT = process.env.PORT || 3001;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
